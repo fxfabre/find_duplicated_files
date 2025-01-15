dd675eb8cc7faa414928c61352a9f3aa4db979a4
Francois-Xavier Fabre <fxfabre@users.noreply.github.com>
2025-01-15 17:27:19 +0100

    script

---
+++ rsync_drive.sh
@@ -0,0 +1,12 @@
+rsync -aXhv --delete /media/francixtra/Archive_FX/Chantal /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Delphine_ro /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Etienne /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/FX /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/PO /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Manu /media/francixtra/rsync_backup/
+
+rsync -aXhv --delete /media/francixtra/Archive_FX/Jeux /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Musique /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Outils_logiciels /media/francixtra/rsync_backup/
+rsync -aXhv --delete /media/francixtra/Archive_FX/Videos /media/francixtra/rsync_backup/
+
